e9615c6df20f9f84c9b68d3d1b940844c5e008b0
Tetsuya <ogwtetsuya@gmail.com>
2021-03-20 22:52:01 +0900

    geojsonに変換するようの関数をつくる

--- json2geojson.go
+++ json2geojson.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-"encoding/json"
-"bytes"
-"fmt"
-"os"
-"io/ioutil"
-"log"
-"net"
-"net/http"
-"time"
-"strconv"
-"./src"
-"./setting"
-
+	"./src"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+	//"./setting"
 )
 
 //このへん参照
@@ -21,10 +20,10 @@ import (
 func EncodingJSON(geojson format.Geojson_data) []byte {
 	bdata, err := json.Marshal(geojson)
 
-		if err != nil {
-			fmt.Println(err)
-			return nil
-		}
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
 	return bdata
 }
 
@@ -34,93 +33,152 @@ func dialTimeout(network, addr string) (net.Conn, error) {
 	return net.DialTimeout(network, addr, timeout)
 }
 
+//json 2 geojson format
+func convert_json_format(flight_data format.Full_data) format.Geojson_data {
+	var All_geojson_flight_data format.Geojson_data
+	var single_geojson_flight_data format.Geojson_Features
+
+	// geojsonに投入
+	for _, single_flight := range flight_data.All_flightdata {
+		//Geometry
+		single_geojson_flight_data.Geometry.Coordinates[0] = single_flight.Lon
+		single_geojson_flight_data.Geometry.Coordinates[1] = single_flight.Lat
+		single_geojson_flight_data.Geometry.Type = "Point"
+		//Type
+		single_geojson_flight_data.Type = "Feature"
+		//Properties
+		single_geojson_flight_data.Properties.Timestamp = flight_data.Timestamp
+		single_geojson_flight_data.Properties.Hex = single_flight.Hex
+		single_geojson_flight_data.Properties.Flight = single_flight.Flight
+		single_geojson_flight_data.Properties.Alt_baro = single_flight.Alt_baro
+		single_geojson_flight_data.Properties.Alt_geom = single_flight.Alt_geom
+		single_geojson_flight_data.Properties.Gs = single_flight.Gs
+		single_geojson_flight_data.Properties.Ias = single_flight.Ias
+		single_geojson_flight_data.Properties.Tas = single_flight.Tas
+		single_geojson_flight_data.Properties.Mach = single_flight.Mach
+		single_geojson_flight_data.Properties.Track = single_flight.Track
+		single_geojson_flight_data.Properties.Track_rate = single_flight.Track_rate
+		single_geojson_flight_data.Properties.Roll = single_flight.Roll
+		single_geojson_flight_data.Properties.Mag_heading = single_flight.Mag_heading
+		single_geojson_flight_data.Properties.Baro_rate = single_flight.Baro_rate
+		single_geojson_flight_data.Properties.Geom_rate = single_flight.Geom_rate
+		single_geojson_flight_data.Properties.Squawk = single_flight.Squawk
+		single_geojson_flight_data.Properties.Emergency = single_flight.Emergency
+		single_geojson_flight_data.Properties.Category = single_flight.Category
+		single_geojson_flight_data.Properties.Nav_qnh = single_flight.Nav_qnh
+		single_geojson_flight_data.Properties.Nav_altitude_mcp = single_flight.Nav_altitude_mcp
+		single_geojson_flight_data.Properties.Nav_altitude_fms = single_flight.Nav_altitude_fms
+		single_geojson_flight_data.Properties.Nav_heading = single_flight.Nav_heading
+		single_geojson_flight_data.Properties.Nic = single_flight.Nic
+		single_geojson_flight_data.Properties.Rc = single_flight.Rc
+		single_geojson_flight_data.Properties.Seen_pos = single_flight.Seen_pos
+		single_geojson_flight_data.Properties.Version = single_flight.Version
+		single_geojson_flight_data.Properties.Nic_baro = single_flight.Nic_baro
+		single_geojson_flight_data.Properties.Nac_p = single_flight.Nac_p
+		single_geojson_flight_data.Properties.Nac_v = single_flight.Nac_v
+		single_geojson_flight_data.Properties.Sil = single_flight.Sil
+		single_geojson_flight_data.Properties.Sil_type = single_flight.Sil_type
+		single_geojson_flight_data.Properties.Gva = single_flight.Gva
+		single_geojson_flight_data.Properties.Sda = single_flight.Sda
+		single_geojson_flight_data.Properties.Messages = single_flight.Messages
+		single_geojson_flight_data.Properties.Seen = single_flight.Seen
+		single_geojson_flight_data.Properties.Rssi = single_flight.Rssi
+		//	fmt.Println(single_geojson_flight_data)
+		All_geojson_flight_data.Features = append(All_geojson_flight_data.Features, single_geojson_flight_data)
+	}
+	All_geojson_flight_data.Type = "FeatureCollection"
+
+	return All_geojson_flight_data
+}
+
 func main() {
 
-//POST先のURL
-const URL = url.Post_target
+	//POST先のURL
+	const URL = "http://localhost:8080"
 
-// JSONファイル読み込み
-json_bytes, err := ioutil.ReadFile("/var/run/dump1090-fa/aircraft.json")
-//json_bytes, err := ioutil.ReadFile("./sample.json")
+	// JSONファイル読み込み
+	//json_bytes, err := ioutil.ReadFile("/var/run/dump1090-fa/aircraft.json")
+	json_bytes, err := ioutil.ReadFile("./sample.json")
 
-if err != nil {
-log.Fatal(err)
-}
-// JSONデコード
-var flight_data format.Full_data
-if err := json.Unmarshal(json_bytes, &flight_data); err != nil {
-log.Fatal(err)
-}
+	if err != nil {
+		log.Fatal(err)
+	}
+	// JSONデコード
+	var flight_data format.Full_data
+	if err := json.Unmarshal(json_bytes, &flight_data); err != nil {
+		log.Fatal(err)
+	}
 
-var single_geojson_flight_data format.Geojson_Features
-var All_geojson_flight_data format.Geojson_data
+	var single_geojson_flight_data format.Geojson_Features
+	var All_geojson_flight_data format.Geojson_data
 
-// geojsonに投入
-for _, single_flight := range flight_data.All_flightdata {
-	//Geometry
-	single_geojson_flight_data.Geometry.Coordinates[0]=single_flight.Lon
-	single_geojson_flight_data.Geometry.Coordinates[1]=single_flight.Lat
-	single_geojson_flight_data.Geometry.Type="Point"
+	// geojsonに投入
+	for _, single_flight := range flight_data.All_flightdata {
+		//Geometry
+		single_geojson_flight_data.Geometry.Coordinates[0] = single_flight.Lon
+		single_geojson_flight_data.Geometry.Coordinates[1] = single_flight.Lat
+		single_geojson_flight_data.Geometry.Type = "Point"
 
-	//Type
-	single_geojson_flight_data.Type="Feature"
-	//Properties
-	single_geojson_flight_data.Properties.Timestamp        =flight_data.Timestamp
-	single_geojson_flight_data.Properties.Hex              =single_flight.Hex
-	single_geojson_flight_data.Properties.Flight           =single_flight.Flight
-	single_geojson_flight_data.Properties.Alt_baro         =single_flight.Alt_baro
-	single_geojson_flight_data.Properties.Alt_geom         =single_flight.Alt_geom
-	single_geojson_flight_data.Properties.Gs               =single_flight.Gs
-	single_geojson_flight_data.Properties.Ias              =single_flight.Ias
-	single_geojson_flight_data.Properties.Tas              =single_flight.Tas
-	single_geojson_flight_data.Properties.Mach             =single_flight.Mach
-	single_geojson_flight_data.Properties.Track            =single_flight.Track
-	single_geojson_flight_data.Properties.Track_rate       =single_flight.Track_rate
-	single_geojson_flight_data.Properties.Roll             =single_flight.Roll
-	single_geojson_flight_data.Properties.Mag_heading      =single_flight.Mag_heading
-	single_geojson_flight_data.Properties.Baro_rate        =single_flight.Baro_rate
-	single_geojson_flight_data.Properties.Geom_rate        =single_flight.Geom_rate
-	single_geojson_flight_data.Properties.Squawk           =single_flight.Squawk
-	single_geojson_flight_data.Properties.Emergency        =single_flight.Emergency
-	single_geojson_flight_data.Properties.Category         =single_flight.Category
-	single_geojson_flight_data.Properties.Nav_qnh          =single_flight.Nav_qnh
-	single_geojson_flight_data.Properties.Nav_altitude_mcp =single_flight.Nav_altitude_mcp
-	single_geojson_flight_data.Properties.Nav_altitude_fms =single_flight.Nav_altitude_fms
-	single_geojson_flight_data.Properties.Nav_heading      =single_flight.Nav_heading
-	single_geojson_flight_data.Properties.Nic              =single_flight.Nic
-	single_geojson_flight_data.Properties.Rc               =single_flight.Rc
-	single_geojson_flight_data.Properties.Seen_pos         =single_flight.Seen_pos
-	single_geojson_flight_data.Properties.Version          =single_flight.Version
-	single_geojson_flight_data.Properties.Nic_baro         =single_flight.Nic_baro
-	single_geojson_flight_data.Properties.Nac_p            =single_flight.Nac_p
-	single_geojson_flight_data.Properties.Nac_v            =single_flight.Nac_v
-	single_geojson_flight_data.Properties.Sil              =single_flight.Sil
-	single_geojson_flight_data.Properties.Sil_type         =single_flight.Sil_type
-	single_geojson_flight_data.Properties.Gva	            =single_flight.Gva
-	single_geojson_flight_data.Properties.Sda	            =single_flight.Sda
-	single_geojson_flight_data.Properties.Messages         =single_flight.Messages
-	single_geojson_flight_data.Properties.Seen             =single_flight.Seen
-	single_geojson_flight_data.Properties.Rssi             =single_flight.Rssi
-//	fmt.Println(single_geojson_flight_data)
-	All_geojson_flight_data.Features = append(All_geojson_flight_data.Features,single_geojson_flight_data)
-}
-	All_geojson_flight_data.Type="FeatureCollection"
+		//Type
+		single_geojson_flight_data.Type = "Feature"
+		//Properties
+		single_geojson_flight_data.Properties.Timestamp = flight_data.Timestamp
+		single_geojson_flight_data.Properties.Hex = single_flight.Hex
+		single_geojson_flight_data.Properties.Flight = single_flight.Flight
+		single_geojson_flight_data.Properties.Alt_baro = single_flight.Alt_baro
+		single_geojson_flight_data.Properties.Alt_geom = single_flight.Alt_geom
+		single_geojson_flight_data.Properties.Gs = single_flight.Gs
+		single_geojson_flight_data.Properties.Ias = single_flight.Ias
+		single_geojson_flight_data.Properties.Tas = single_flight.Tas
+		single_geojson_flight_data.Properties.Mach = single_flight.Mach
+		single_geojson_flight_data.Properties.Track = single_flight.Track
+		single_geojson_flight_data.Properties.Track_rate = single_flight.Track_rate
+		single_geojson_flight_data.Properties.Roll = single_flight.Roll
+		single_geojson_flight_data.Properties.Mag_heading = single_flight.Mag_heading
+		single_geojson_flight_data.Properties.Baro_rate = single_flight.Baro_rate
+		single_geojson_flight_data.Properties.Geom_rate = single_flight.Geom_rate
+		single_geojson_flight_data.Properties.Squawk = single_flight.Squawk
+		single_geojson_flight_data.Properties.Emergency = single_flight.Emergency
+		single_geojson_flight_data.Properties.Category = single_flight.Category
+		single_geojson_flight_data.Properties.Nav_qnh = single_flight.Nav_qnh
+		single_geojson_flight_data.Properties.Nav_altitude_mcp = single_flight.Nav_altitude_mcp
+		single_geojson_flight_data.Properties.Nav_altitude_fms = single_flight.Nav_altitude_fms
+		single_geojson_flight_data.Properties.Nav_heading = single_flight.Nav_heading
+		single_geojson_flight_data.Properties.Nic = single_flight.Nic
+		single_geojson_flight_data.Properties.Rc = single_flight.Rc
+		single_geojson_flight_data.Properties.Seen_pos = single_flight.Seen_pos
+		single_geojson_flight_data.Properties.Version = single_flight.Version
+		single_geojson_flight_data.Properties.Nic_baro = single_flight.Nic_baro
+		single_geojson_flight_data.Properties.Nac_p = single_flight.Nac_p
+		single_geojson_flight_data.Properties.Nac_v = single_flight.Nac_v
+		single_geojson_flight_data.Properties.Sil = single_flight.Sil
+		single_geojson_flight_data.Properties.Sil_type = single_flight.Sil_type
+		single_geojson_flight_data.Properties.Gva = single_flight.Gva
+		single_geojson_flight_data.Properties.Sda = single_flight.Sda
+		single_geojson_flight_data.Properties.Messages = single_flight.Messages
+		single_geojson_flight_data.Properties.Seen = single_flight.Seen
+		single_geojson_flight_data.Properties.Rssi = single_flight.Rssi
+		//	fmt.Println(single_geojson_flight_data)
+		All_geojson_flight_data.Features = append(All_geojson_flight_data.Features, single_geojson_flight_data)
+	}
+	All_geojson_flight_data.Type = "FeatureCollection"
 
-//	fmt.Println(All_geojson_flight_data)
+	//	fmt.Println(All_geojson_flight_data)
 
 	bdata := EncodingJSON(All_geojson_flight_data)
 
-//POST
-	res, err := http.Post(URL, "application/json",bytes.NewBuffer(bdata))
+	//POST
+	res, err := http.Post(URL, "application/json", bytes.NewBuffer(bdata))
 	defer res.Body.Close()
 	if err != nil {
-	//IF TIMEOUT save geojson
+		//IF TIMEOUT save geojson
 		nowUTC := time.Now().UTC()
-		millisecond:=nowUTC.UnixNano() / int64(time.Millisecond)
-		geojson_name:="/home/gunma/workplace/adsb/"+strconv.FormatInt(millisecond, 10)+".geojson"
-		ioutil.WriteFile(geojson_name,[]byte(bdata), os.ModePerm)
+		millisecond := nowUTC.UnixNano() / int64(time.Millisecond)
+		geojson_name := "/home/gunma/workplace/adsb/" + strconv.FormatInt(millisecond, 10) + ".geojson"
+		ioutil.WriteFile(geojson_name, []byte(bdata), os.ModePerm)
 		fmt.Println("[!] " + err.Error())
 	} else {
 		fmt.Println("[*] " + res.Status)
 	}
+
 }
--- src/json_formts.go
+++ src/json_formts.go
@@ -1,102 +1,102 @@
 package format
-/** JSONデコード用に構造体定義 */
+
+// JSONデコード用に構造体定義 //
 type Full_data struct {
-	Timestamp float64 `json:"now"`
-	Messages  uint32 `json:"messages"`
+	Timestamp      float64 `json:"now"`
+	Messages       uint32  `json:"messages"`
 	All_flightdata []struct {
-		Hex                string `json:"hex"`
-		Flight             string `json:"flight,omitempty"`
-		Alt_baro              int `json:"alt_baro"`
-		Alt_geom              int `json:"alt_geom,omitempty"`
-		Gs                float32 `json:"gs,omitempty"`
-		Ias                   int `json:"ias,omitempty"`
-		Tas                   int `json:"tas,omitempty"`
-		Mach              float32 `json:"mach,omitempty"`
-		Track             float32 `json:"track,omitempty"`
-		Track_rate        float32 `json:"track_rate,omitempty"`
-		Roll              float32 `json:"roll,omitempty"`
-		Mag_heading       float32 `json:"mag_heading,omitempty"`
-		Baro_rate             int `json:"mag_heading,omitempty"`
-		Geom_rate             int `json:"geom_rate,omitempty"`
-		Squawk             string `json:"squawk,omitempty"`
-		Emergency          string `json:"emergency,omitempty"`
-		Category           string `json:"category,omitempty"`
-		Nav_qnh           float32 `json:"nav_qnh,omitempty"`
-		Nav_altitude_mcp      int `json:"nav_altitude_mcp,omitempty"`
-		Nav_altitude_fms      int `json:"nav_altitude_fms,omitempty"`
-		Nav_heading       float32 `json:"nav_heading,omitempty"`
-		Lat               float32 `json:"lat,omitempty"`
-		Lon               float32 `json:"lon,omitempty"`
-		Nic                   int `json:"nic,omitempty"`
-		Rc                    int `json:"rc,omitempty"`
-		Seen_pos          float32 `json:"seen_pos,omitempty"`
-		Version           float32 `json:"version,omitempty"`
-		Nic_baro              int `json:"nic_baro,omitempty"`
-		Nac_p                 int `json:"nac_p,omitempty"`
-		Nac_v                 int `json:"nac_v,omitempty"`
-		Sil                   int `json:"sil,omitempty"`
-		Sil_type           string `json:"sil_type,omitempty"`
-		Gva	                  int `json:"gva,omitempty"`
-		Sda	                  int `json:"sda,omitempty"`
-		Messages              int `json:"messages,omitempty"`
-		Seen              float32 `json:"seen,omitempty"`
-		Rssi              float32 `json:"rssi,omitempty"`
-	}	`json:"aircraft"`
+		Hex              string  `json:"hex"`
+		Flight           string  `json:"flight,omitempty"`
+		Alt_baro         int     `json:"alt_baro"`
+		Alt_geom         int     `json:"alt_geom,omitempty"`
+		Gs               float32 `json:"gs,omitempty"`
+		Ias              int     `json:"ias,omitempty"`
+		Tas              int     `json:"tas,omitempty"`
+		Mach             float32 `json:"mach,omitempty"`
+		Track            float32 `json:"track,omitempty"`
+		Track_rate       float32 `json:"track_rate,omitempty"`
+		Roll             float32 `json:"roll,omitempty"`
+		Mag_heading      float32 `json:"mag_heading,omitempty"`
+		Baro_rate        int     `json:"mag_heading,omitempty"`
+		Geom_rate        int     `json:"geom_rate,omitempty"`
+		Squawk           string  `json:"squawk,omitempty"`
+		Emergency        string  `json:"emergency,omitempty"`
+		Category         string  `json:"category,omitempty"`
+		Nav_qnh          float32 `json:"nav_qnh,omitempty"`
+		Nav_altitude_mcp int     `json:"nav_altitude_mcp,omitempty"`
+		Nav_altitude_fms int     `json:"nav_altitude_fms,omitempty"`
+		Nav_heading      float32 `json:"nav_heading,omitempty"`
+		Lat              float32 `json:"lat,omitempty"`
+		Lon              float32 `json:"lon,omitempty"`
+		Nic              int     `json:"nic,omitempty"`
+		Rc               int     `json:"rc,omitempty"`
+		Seen_pos         float32 `json:"seen_pos,omitempty"`
+		Version          float32 `json:"version,omitempty"`
+		Nic_baro         int     `json:"nic_baro,omitempty"`
+		Nac_p            int     `json:"nac_p,omitempty"`
+		Nac_v            int     `json:"nac_v,omitempty"`
+		Sil              int     `json:"sil,omitempty"`
+		Sil_type         string  `json:"sil_type,omitempty"`
+		Gva              int     `json:"gva,omitempty"`
+		Sda              int     `json:"sda,omitempty"`
+		Messages         int     `json:"messages,omitempty"`
+		Seen             float32 `json:"seen,omitempty"`
+		Rssi             float32 `json:"rssi,omitempty"`
+	} `json:"aircraft"`
 }
 
 //geojson用の構造体
 type Geojson_Geometry struct {
 	Coordinates [2]float32 `json:"coordinates"`
-	Type            string    `json:"type"`
+	Type        string     `json:"type"`
 }
 
-type Geojson_Features struct{
-	Type         string            `json:"type"`
-	Geometry     Geojson_Geometry  `json:"geometry"`
-	Properties   Geojson_Properties`json:"properties"`
+type Geojson_Features struct {
+	Type       string             `json:"type"`
+	Geometry   Geojson_Geometry   `json:"geometry"`
+	Properties Geojson_Properties `json:"properties"`
 }
 
 type Geojson_Properties struct {
-Timestamp         float64 `json:"timestamp"`
-Hex                string `json:"hex"`
-Flight             string `json:"flight,omitempty"`
-Alt_baro              int `json:"alt_baro"`
-Alt_geom              int `json:"alt_geom,omitempty"`
-Gs                float32 `json:"gs,omitempty"`
-Ias                   int `json:"ias,omitempty"`
-Tas                   int `json:"tas,omitempty"`
-Mach              float32 `json:"mach,omitempty"`
-Track             float32 `json:"track,omitempty"`
-Track_rate        float32 `json:"track_rate,omitempty"`
-Roll              float32 `json:"roll,omitempty"`
-Mag_heading       float32 `json:"mag_heading,omitempty"`
-Baro_rate             int `json:"mag_heading,omitempty"`
-Geom_rate             int `json:"geom_rate,omitempty"`
-Squawk             string `json:"squawk,omitempty"`
-Emergency          string `json:"emergency,omitempty"`
-Category           string `json:"category,omitempty"`
-Nav_qnh           float32 `json:"nav_qnh,omitempty"`
-Nav_altitude_mcp      int `json:"nav_altitude_mcp,omitempty"`
-Nav_altitude_fms      int `json:"nav_altitude_fms,omitempty"`
-Nav_heading       float32 `json:"nav_heading,omitempty"`
-Nic                   int `json:"nic,omitempty"`
-Rc                    int `json:"rc,omitempty"`
-Seen_pos          float32 `json:"seen_pos,omitempty"`
-Version           float32 `json:"version,omitempty"`
-Nic_baro              int `json:"nic_baro,omitempty"`
-Nac_p                 int `json:"nac_p,omitempty"`
-Nac_v                 int `json:"nac_v,omitempty"`
-Sil                   int `json:"sil,omitempty"`
-Sil_type           string `json:"sil_type,omitempty"`
-Gva	                  int `json:"gva,omitempty"`
-Sda	                  int `json:"sda,omitempty"`
-Messages              int `json:"messages,omitempty"`
-Seen              float32 `json:"seen,omitempty"`
-Rssi              float32 `json:"rssi,omitempty"`
+	Timestamp        float64 `json:"timestamp"`
+	Hex              string  `json:"hex"`
+	Flight           string  `json:"flight,omitempty"`
+	Alt_baro         int     `json:"alt_baro"`
+	Alt_geom         int     `json:"alt_geom,omitempty"`
+	Gs               float32 `json:"gs,omitempty"`
+	Ias              int     `json:"ias,omitempty"`
+	Tas              int     `json:"tas,omitempty"`
+	Mach             float32 `json:"mach,omitempty"`
+	Track            float32 `json:"track,omitempty"`
+	Track_rate       float32 `json:"track_rate,omitempty"`
+	Roll             float32 `json:"roll,omitempty"`
+	Mag_heading      float32 `json:"mag_heading,omitempty"`
+	Baro_rate        int     `json:"mag_heading,omitempty"`
+	Geom_rate        int     `json:"geom_rate,omitempty"`
+	Squawk           string  `json:"squawk,omitempty"`
+	Emergency        string  `json:"emergency,omitempty"`
+	Category         string  `json:"category,omitempty"`
+	Nav_qnh          float32 `json:"nav_qnh,omitempty"`
+	Nav_altitude_mcp int     `json:"nav_altitude_mcp,omitempty"`
+	Nav_altitude_fms int     `json:"nav_altitude_fms,omitempty"`
+	Nav_heading      float32 `json:"nav_heading,omitempty"`
+	Nic              int     `json:"nic,omitempty"`
+	Rc               int     `json:"rc,omitempty"`
+	Seen_pos         float32 `json:"seen_pos,omitempty"`
+	Version          float32 `json:"version,omitempty"`
+	Nic_baro         int     `json:"nic_baro,omitempty"`
+	Nac_p            int     `json:"nac_p,omitempty"`
+	Nac_v            int     `json:"nac_v,omitempty"`
+	Sil              int     `json:"sil,omitempty"`
+	Sil_type         string  `json:"sil_type,omitempty"`
+	Gva              int     `json:"gva,omitempty"`
+	Sda              int     `json:"sda,omitempty"`
+	Messages         int     `json:"messages,omitempty"`
+	Seen             float32 `json:"seen,omitempty"`
+	Rssi             float32 `json:"rssi,omitempty"`
 }
 
 type Geojson_data struct {
-	Type string `json:"type"`
-	Features []Geojson_Features`json:"features"`
+	Type     string             `json:"type"`
+	Features []Geojson_Features `json:"features"`
 }
-
